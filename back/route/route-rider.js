98d0e53e274d8e3b84527d801d0221990949c8b6
Melanie Downing <sayanything830@gmail.com>
2018-04-23 14:34:42 -0700

    finishing routes

--- back/route/route-rider.js
+++ back/route/route-rider.js
@@ -18,7 +18,30 @@ module.exports = function(router) {
         .catch(error => errorHandler(error, response));
     })
 
-    .post()
-    .put()
-    .delete();
+    .post(bodyParser, (request, response) => {
+      new Rider(request.body).save()
+        .then(rider => response.status(201).json(rider))
+        .catch(error => errorHandler(error, response));
+    })
+    .put(bodyParser, (request, response) => {
+      Rider.findOne({
+        id: request.params._id,
+      })
+        .then(rider => {
+          if(!rider) return Promise.reject(new Error('No Rider Found'));
+          return rider.set(request.body).save();
+        })
+        .then(() => response.sendStatus(204))
+        .catch(error => errorHandler(error, response));
+    })
+    .delete((request, response) => {
+      return Rider.findById(request.params._id)
+        .then(rider => {
+          if(rider) return rider.remove();
+          return errorHandler(new Error('Delete Failed, unable to find rider'), response);
+        })
+        .then(() => response.sendStatus(204))
+        .catch(error => errorHandler(error, response));
+    });
 };
+
